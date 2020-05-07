0716846b3450ac367f134727a5a44073d59d51da
stassss2011 <stassss2011@https://users.noreply.github.com/>
2020-05-07 22:20:06 +0000

    Update pages on 2020-05-07 22:20:06

--- js/main-min.js
+++ js/main-min.js
@@ -1,2 +1,2 @@
-
+const datesInfo=document.getElementById("for-select"),select=document.getElementById("select");function fakeChange(){datesInfo.style.opacity=0,setTimeout(()=>datesInfo.style.opacity=1,500)}select.addEventListener("input",fakeChange);
 //# sourceMappingURL=main-min.js.map
--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+const datesInfo = document.getElementById('for-select')
+const select = document.getElementById('select')
 
+select.addEventListener('input', fakeChange)
+
+function fakeChange() {
+    datesInfo.style.opacity = 0
+    setTimeout(() => datesInfo.style.opacity = 1, 500)
+}
 //# sourceMappingURL=main.js.map
