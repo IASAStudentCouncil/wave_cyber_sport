0f5f034dfd0a5855e3aca9bb6366db07a071bc6f
koval2001 <dianakoval2001@icloud.com>
2020-05-11 15:15:29 +0300

    switch backs and partners

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,15 @@ function fakeChange() {
     datesInfo.style.opacity = 0
     setTimeout(() => datesInfo.style.opacity = 1, 500)
 }
+
+function changeDota() {
+    document.getElementById("welcome").style.backgroundImage = "url('../img/dota.jpg')";
+    document.getElementById("btn-dota").style.display= "none";
+    document.getElementById("btn-csgo").style.display = "flex";
+}
+
+function changeCs() {
+    document.getElementById("welcome").style.backgroundImage = "url('../img/csgo.jpg')";
+    document.getElementById("btn-dota").style.display= "flex";
+    document.getElementById("btn-csgo").style.display = "none";
+}
