3fcd5e856c20185ab170d33f74b9a3891e8ba165
koval2001 <koval2001@https://users.noreply.github.com/>
2020-05-11 12:16:56 +0000

    Update pages on 2020-05-11 12:16:56

--- js/main.js
+++ js/main.js
@@ -7,4 +7,16 @@ function fakeChange() {
     datesInfo.style.opacity = 0
     setTimeout(() => datesInfo.style.opacity = 1, 500)
 }
+
+function changeDota() {
+    document.getElementById("welcome").style.backgroundImage = "url('../img/dota.jpg')";
+    document.getElementById("btn-dota").style.display= "none";
+    document.getElementById("btn-csgo").style.display = "flex";
+}
+
+function changeCs() {
+    document.getElementById("welcome").style.backgroundImage = "url('../img/csgo.jpg')";
+    document.getElementById("btn-dota").style.display= "flex";
+    document.getElementById("btn-csgo").style.display = "none";
+}
 //# sourceMappingURL=main.js.map
